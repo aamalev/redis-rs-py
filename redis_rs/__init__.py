705f8ececab3c288c97a5ed588731703863a15b9
Alexander Malev <malev@somedev.ru>
2023-10-15 20:52:38 +0300

    Improve __init__.py

--- redis_rs/__init__.py
+++ redis_rs/__init__.py
@@ -1,1 +1,10 @@
-from .redis_rs import *
+__all__ = [
+    "create_client",
+    "Client",
+    "AsyncClient",
+    "exceptions",
+]
+
+from .redis_rs import Client, create_client, exceptions
+
+AsyncClient = Client
