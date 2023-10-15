3c00ec10b3b1a4b69422dc23c417492ee42954af
Alexander Malev <aamalev@users.noreply.github.com>
2023-10-15 21:51:55 +0300

    Add pytest (#7)
    * Add pytest
    
    * pytest on ci

---
+++ tests/test_exc.py
@@ -0,0 +1,7 @@
+import pytest
+import redis_rs
+
+
+async def test_redis_error(async_client: redis_rs.AsyncClient):
+    with pytest.raises(redis_rs.exceptions.RedisError):
+        await async_client.fetch_int("CLUSTER", "SLOTS")
