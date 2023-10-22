d394b4888d6afc9dddec79b5adbd6a0fc8d00c05
Alexander Malev <aamalev@users.noreply.github.com>
2023-10-22 23:20:53 +0300

    Improve to_dict (#14)
    * Improve to_dict
    * Extract version

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,57 @@
+import asyncio
+import os
+
 import pytest
 
 import redis_rs
 
 
+def to_addr(s: str) -> str:
+    if s.isdigit():
+        return f"redis://localhost:{s}"
+    elif not s.startswith("redis://"):
+        return f"redis://{s}"
+    return s
+
+
+async def get_redis_version(nodes: list) -> str:
+    key = "redis_version"
+    async with redis_rs.create_client(
+        *nodes,
+    ) as c:
+        infos = await c.execute("INFO", "SERVER", encoding="info")
+        if isinstance(infos, dict):
+            result = infos.get(key)
+        else:
+            infos = filter(lambda x: isinstance(x, dict), infos)
+            result = min(map(lambda x: x[key], infos))
+        return result
+
+
+NODES = [to_addr(node) for node in os.environ.get("REDIS_NODES", "").split(",") if node]
+IS_CLUSTER = os.environ.get("REDIS_CLUSTER", "0") not in {"0"}
+VERSION = ""
+
+
 @pytest.fixture
 async def async_client():
-    async with redis_rs.create_client() as c:
+    async with redis_rs.create_client(
+        *NODES,
+        cluster=IS_CLUSTER,
+    ) as c:
         yield c
+
+
+def pytest_runtest_setup(item):
+    for marker in item.iter_markers():
+        if marker.name == "redis":
+            if marker.kwargs.get("cluster") and not IS_CLUSTER:
+                pytest.skip("Single redis")
+            elif marker.kwargs.get("single") and IS_CLUSTER:
+                pytest.skip("Cluster redis")
+            elif version := marker.kwargs.get("version"):
+                global VERSION
+                if not VERSION:
+                    VERSION = asyncio.run(get_redis_version(NODES))
+                if str(version) > VERSION:
+                    pytest.skip(f"redis_version:{VERSION} < {version}")
