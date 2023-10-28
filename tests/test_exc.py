02fc23b86b33468cc9fcdb3bebecf5d5d310719d
Alexander Malev <malev@somedev.ru>
2023-10-29 01:25:29 +0300

    improve test

--- tests/test_exc.py
+++ tests/test_exc.py
@@ -1,9 +1,9 @@
 import pytest
 
-import redis_rs.client_async
+from redis_rs import AsyncClient
 from redis_rs.exceptions import RedisError
 
 
-async def test_redis_error(async_client: redis_rs.client_async.AsyncClient):
+async def test_redis_error(async_client: AsyncClient):
     with pytest.raises(RedisError):
         await async_client.fetch_int("CLUSTER", "SLOTS")
