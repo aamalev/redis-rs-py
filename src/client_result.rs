28bf172ea348a0ec13b400d9cdd73b4acffae6aa
Alexander Malev <malev@somedev.ru>
2023-11-17 01:45:22 +0300

    types::Codec

--- src/client_result.rs
+++ src/client_result.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use pyo3::prelude::*;
 use redis::{Cmd, Value};
 
-use crate::{client::Client, error};
+use crate::{client::Client, error, types};
 
 pub trait ClientResult {
     fn init<'a>(&self, py: Python<'a>, client: &Client) -> PyResult<&'a PyAny>;
@@ -15,11 +15,16 @@ pub trait ClientResult {
         traceback: &PyAny,
     ) -> PyResult<&'a PyAny>;
     fn status(&self) -> Result<HashMap<String, Value>, error::RedisError>;
-    fn execute<'a>(&self, py: Python<'a>, cmd: Cmd, encoding: String) -> PyResult<&'a PyAny>;
+    fn execute<'a>(&self, py: Python<'a>, cmd: Cmd, encoding: types::Codec) -> PyResult<&'a PyAny>;
     fn fetch_str<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny>;
     fn fetch_bytes<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny>;
     fn fetch_list<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny>;
-    fn fetch_dict<'a>(&self, py: Python<'a>, cmd: Cmd, encoding: String) -> PyResult<&'a PyAny>;
+    fn fetch_dict<'a>(
+        &self,
+        py: Python<'a>,
+        cmd: Cmd,
+        encoding: types::Codec,
+    ) -> PyResult<&'a PyAny>;
     fn fetch_scores<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny>;
     fn fetch_bool<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny>;
     fn fetch_int<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny>;
