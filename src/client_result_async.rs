996a247f3a40129542c0808aeec4edfe7e020a36
Alexander Malev <malev@somedev.ru>
2025-01-21 02:16:27 +0300

    Use native async with pyo3

--- src/client_result_async.rs
+++ src/client_result_async.rs
@@ -1,135 +1,73 @@
 use std::{collections::HashMap, sync::Arc};
 
-use pyo3::{prelude::*, types::PyBytes};
-use pyo3_asyncio::tokio::future_into_py;
-use redis::{Cmd, Value};
+use pyo3::prelude::*;
+use redis::{Cmd, FromRedisValue, Value};
 
-use crate::{client::Client, client_result::ClientResult, error, pool_manager::PoolManager, types};
+use crate::{client_async::Client, error, pool_manager::PoolManager, types};
 
 #[derive(Clone)]
 pub struct AsyncClientResult {
     pub(crate) cm: Arc<tokio::sync::RwLock<PoolManager>>,
 }
 
-impl ClientResult for AsyncClientResult {
-    fn init<'a>(&self, py: Python<'a>, client: &Client) -> PyResult<&'a PyAny> {
+fn tokio_rt() -> &'static tokio::runtime::Runtime {
+    use std::sync::OnceLock;
+    static RT: OnceLock<tokio::runtime::Runtime> = OnceLock::new();
+    RT.get_or_init(|| tokio::runtime::Runtime::new().unwrap())
+}
+
+impl AsyncClientResult {
+    pub async fn init(&self, client: &Client) -> PyResult<Client> {
         let cm = self.cm.clone();
-        let client_id = client.client_id.clone();
-        future_into_py(py, async move {
-            cm.write().await.init().await?;
-            let result = Client {
-                cr: Box::new(Self { cm }),
-                client_id,
-            };
-            Ok(result)
-        })
+        tokio_rt()
+            .spawn(async move { cm.write().await.init().await })
+            .await
+            .unwrap()?;
+        Ok(client.clone())
     }
 
-    fn close<'a>(
-        &self,
-        py: Python<'a>,
-        _exc_type: &PyAny,
-        _exc_value: &PyAny,
-        _traceback: &PyAny,
-    ) -> PyResult<&'a PyAny> {
+    pub async fn close(&self) -> PyResult<()> {
         let cm = self.cm.clone();
-        future_into_py(py, async move {
-            cm.write().await.close().await;
-            Ok(())
-        })
+        tokio_rt()
+            .spawn(async move { cm.write().await.close().await })
+            .await
+            .unwrap();
+        Ok(())
     }
 
-    fn status(&self) -> Result<HashMap<String, Value>, error::RedisError> {
+    pub fn status(&self) -> Result<HashMap<String, Value>, error::RedisError> {
         let cm = self.cm.try_read()?;
         let result = cm.status();
         Ok(result)
     }
 
-    fn execute<'a>(&self, py: Python<'a>, cmd: Cmd, encoding: types::Codec) -> PyResult<&'a PyAny> {
+    pub async fn execute(&self, cmd: Cmd, encoding: types::Codec) -> PyResult<PyObject> {
         let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let value = cm.pool.execute(cmd).await?;
-            Python::with_gil(|py| types::to_object(py, value, encoding))
-        })
+        let result = tokio_rt()
+            .spawn(async move { cm.read().await.pool.execute(cmd).await })
+            .await
+            .unwrap()?;
+        Python::with_gil(|py| types::to_object(py, result, encoding))
     }
 
-    fn fetch_str<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
+    pub async fn fetch_dict(&self, cmd: Cmd, encoding: types::Codec) -> PyResult<PyObject> {
         let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let result: Option<String> = cm.execute(cmd).await?;
-            Ok(result)
-        })
+        let result = tokio_rt()
+            .spawn(async move { cm.read().await.pool.execute(cmd).await })
+            .await
+            .unwrap()?;
+        Python::with_gil(|py| types::to_dict(py, result, encoding))
     }
 
-    fn fetch_bytes<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
+    pub async fn fetch<T>(&self, cmd: Cmd) -> PyResult<T>
+    where
+        T: FromRedisValue + Send + 'static,
+    {
         let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let result: Vec<u8> = cm.execute(cmd).await?;
-            Ok(Python::with_gil(|py| {
-                PyBytes::new(py, &result).to_object(py)
-            }))
-        })
-    }
-
-    fn fetch_list<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
-        let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let result: Vec<String> = cm.execute(cmd).await?;
-            Ok(result)
-        })
-    }
-
-    fn fetch_dict<'a>(
-        &self,
-        py: Python<'a>,
-        cmd: Cmd,
-        encoding: types::Codec,
-    ) -> PyResult<&'a PyAny> {
-        let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let value = cm.pool.execute(cmd).await?;
-            Python::with_gil(|py| types::to_dict(py, value, encoding))
-        })
-    }
-
-    fn fetch_scores<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
-        let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let map: HashMap<String, f64> = cm.execute(cmd).await?;
-            Ok(Python::with_gil(|py| map.to_object(py)))
-        })
-    }
-
-    fn fetch_bool<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
-        let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let result: bool = cm.execute(cmd).await?;
-            Ok(result)
-        })
-    }
-
-    fn fetch_int<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
-        let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let result: i64 = cm.execute(cmd).await?;
-            Ok(result)
-        })
-    }
-
-    fn fetch_float<'a>(&self, py: Python<'a>, cmd: Cmd) -> PyResult<&'a PyAny> {
-        let cm = self.cm.clone();
-        future_into_py(py, async move {
-            let cm = cm.read().await;
-            let result: f64 = cm.execute(cmd).await?;
-            Ok(result)
-        })
+        let result = tokio_rt()
+            .spawn(async move { cm.read().await.execute(cmd).await })
+            .await
+            .unwrap()?;
+        Ok(result)
     }
 }
--- src/exceptions.rs
+++ src/exceptions.rs
@@ -1,14 +1,8 @@
 use crate::error;
-use pyo3::{create_exception, types::PyModule, PyErr, PyResult, Python};
+use pyo3::{create_exception, PyErr};
 
 create_exception!(exceptions, RedisError, pyo3::exceptions::PyException);
 create_exception!(exceptions, PoolError, pyo3::exceptions::PyException);
-
-pub fn exceptions(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add("RedisError", py.get_type::<RedisError>())?;
-    m.add("PoolError", py.get_type::<PoolError>())?;
-    Ok(())
-}
 
 impl From<error::RedisError> for PyErr {
     fn from(e: error::RedisError) -> Self {
