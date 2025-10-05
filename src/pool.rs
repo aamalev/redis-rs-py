da480ac115ba957b3ad8804ba28de11bfa060cad
Alexander Malev <malev@somedev.ru>
2025-10-05 00:45:39 +0000

    drop unused Connection

--- src/pool.rs
+++ src/pool.rs
@@ -3,34 +3,7 @@ use std::collections::HashMap;
 use crate::{command::Params, error};
 use async_trait::async_trait;
 
-use redis::aio::ConnectionLike;
 use redis::Cmd;
-
-pub struct Connection {
-    pub(crate) c: Box<dyn ConnectionLike + Send>,
-}
-
-impl ConnectionLike for Connection {
-    fn req_packed_command<'a>(
-        &'a mut self,
-        cmd: &'a redis::Cmd,
-    ) -> redis::RedisFuture<'a, redis::Value> {
-        Box::pin(self.c.req_packed_command(cmd))
-    }
-
-    fn req_packed_commands<'a>(
-        &'a mut self,
-        cmd: &'a redis::Pipeline,
-        offset: usize,
-        count: usize,
-    ) -> redis::RedisFuture<'a, Vec<redis::Value>> {
-        Box::pin(self.c.req_packed_commands(cmd, offset, count))
-    }
-
-    fn get_db(&self) -> i64 {
-        self.c.get_db()
-    }
-}
 
 #[async_trait]
 pub trait Pool {
